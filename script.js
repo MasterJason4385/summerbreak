3ea5952d9e83818dcfb1212b10cdcf372990b020
MasterJason4385 <48330547+MasterJason4385@users.noreply.github.com>
2020-03-11 10:56:39 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function tick(){
+	d = new Date();
+  mon = d.getMonth();
+  day = d.getMonth();
+  hr = d.getMonth();
+  min = d.getMonth();
+  sec = d.getMonth();
+  ms = d.getMonth();
+	setTimeout(tick);
+}
